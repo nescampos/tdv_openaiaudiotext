130d8b15d22607d683cc5fe1c37c54526d60d448
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2023-04-11 18:50:55 -0400

    Refactor code for demo

--- Controllers/IncomingAudioController.cs
+++ Controllers/IncomingAudioController.cs
@@ -1,112 +1,108 @@
+using FFMpegCore;
 using Microsoft.AspNetCore.Mvc;
-using Twilio;
 using Twilio.AspNet.Core;
 using Twilio.TwiML;
-using OpenAI.GPT3;
-using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.Interfaces;
 using OpenAI.GPT3.ObjectModels;
 using OpenAI.GPT3.ObjectModels.RequestModels;
 using OpenAI.GPT3.ObjectModels.ResponseModels;
 
 namespace TwilioWhatsAppOpenAI.Controllers;
 
-[ApiController]
 [Route("[controller]")]
 public class IncomingAudioController : TwilioController
 {
-    private const string FilesRootPath = @"<path to WhatsAppAudioFiles folder>";
-    private OpenAIService OpenAIService;
+    private const string FilesRootPath = "WhatsAppAudioFiles";
+    private readonly HttpClient httpClient;
+    private readonly IOpenAIService openAIService;
 
 
-    public IncomingAudioController(IConfiguration configuration)
+    public IncomingAudioController(HttpClient httpClient, IOpenAIService openAIService)
     {
-        TwilioClient.Init(configuration["TwilioAccountSid"], configuration["TwilioAuthToken"]);
-        OpenAIService = new OpenAIService(new OpenAiOptions {ApiKey = configuration["OpenAISecret"] });
+        this.httpClient = httpClient;
+        this.openAIService = openAIService;
     }
 
     [HttpPost]
-    public async Task<IActionResult> Receive()
+    public async Task<IActionResult> Receive(CancellationToken ct)
     {
-        var requestData = HttpContext.Request.Form;
-        var numMedia = requestData["NumMedia"];
-        if(numMedia.Any())
+        var response = new MessagingResponse();
+        var form = await Request.ReadFormAsync(ct);
+        var numMedia = int.Parse(form["NumMedia"].ToString());
+
+        if (numMedia == 0)
         {
-            var messagingResponse = new MessagingResponse();
-            int numMediaValue = int.Parse(numMedia[0]);
-            string transcriptions = string.Empty;
-            for (var i = 0; i < numMediaValue; i++)
-            {
-                
-                var mediaUrl = Request.Form[$"MediaUrl{i}"];
-                var contentType = Request.Form[$"MediaContentType{i}"];
-
-                var filePath = GetMediaFileName(mediaUrl, contentType);
-                DownloadUrlToFileAsync(mediaUrl, filePath).Wait();
-
-                string textFromAudio = await ProcessAudio(filePath);
-                transcriptions += "Transcription for audio: "+textFromAudio;
-            }
-            messagingResponse.Message(transcriptions);
-
-            return TwiML(messagingResponse);
+            response.Message("Please sent an audio file.");
+            return TwiML(response);
         }
-        return Ok();
+        if (numMedia > 1)
+        {
+            response.Message("You can only sent one audio file at a time.");
+            return TwiML(response);
+        }
+
+        var mediaUrl = form["MediaUrl0"];
+        var contentType = form["MediaContentType0"];
+        var extension = GetExtensionFromContentType(contentType);
+        var filePath = Path.Combine(FilesRootPath, Path.GetFileName(mediaUrl) + extension);
+        
+        await DownloadUrlToFileAsync(mediaUrl, filePath, ct);
+
+        var oldFilePath = filePath;
+        filePath = Path.ChangeExtension(filePath, ".wav");
+        ConvertMediaUsingFffmpeg(oldFilePath, filePath);
+        System.IO.File.Delete(oldFilePath);
+        
+        string textFromAudio = await ProcessAudio(filePath);
+        response.Message( $"Transcription for audio: {textFromAudio}");
+        System.IO.File.Delete(filePath);
+        
+        return TwiML(response);
     }
 
-    private string GetMediaFileName(string mediaUrl, string contentType)
+    private async Task DownloadUrlToFileAsync(string mediaUrl, string filePath, CancellationToken ct)
     {
-        return Path.Combine(FilesRootPath, Path.GetFileName(mediaUrl)+GetDefaultExtension(contentType));
+        var response = await httpClient.GetAsync(mediaUrl, ct);
+        var httpStream = await response.Content.ReadAsStreamAsync(ct);
+        await using var fileStream = System.IO.File.Create(filePath);
+        await httpStream.CopyToAsync(fileStream, ct);
+        await fileStream.FlushAsync(ct);
     }
 
-    private static async Task DownloadUrlToFileAsync(string mediaUrl, string filePath)
+    private static string GetExtensionFromContentType(string mimeType)
     {
-        using (var client = new HttpClient())
+        var extension = mimeType switch
         {
-            var response = await client.GetAsync(mediaUrl);
-            var httpStream = await response.Content.ReadAsStreamAsync();
-            using (var fileStream = System.IO.File.Create(filePath))
-            {
-                await httpStream.CopyToAsync(fileStream);
-                await fileStream.FlushAsync();
-            }
-        }
-    }
-
-    private static string GetDefaultExtension(string mimeType)
-    {
-        string extension = string.Empty;
-
-        switch (mimeType)
-        {
-            case "audio/aac":
-                extension = ".aac";
-                break;
-            case "audio/mp4":
-                extension = ".mp4";
-                break;
-            case "audio/mpeg":
-                extension = ".mp3";
-                break;
-            case "audio/wav":
-                extension = ".wav";
-                break;
-            case "audio/webm":
-                extension = ".webm";
-                break;
-            default:
-                throw new ArgumentException($"Mimetype {mimeType} no es un formato de audio válido.");
-        }
+            "audio/aac" => ".aac",
+            "audio/mp4" => ".mp4",
+            "audio/mpeg" => ".mp3",
+            "audio/wav" => ".wav",
+            "audio/webm" => ".webm",
+            "audio/ogg" => ".ogg",
+            _ => throw new ArgumentException($"Mimetype {mimeType} is not supported.")
+        };
         return extension;
     }
 
     private async Task<string> ProcessAudio(string filePath)
     {
         var audioFile = await System.IO.File.ReadAllBytesAsync(filePath);
-        AudioCreateTranscriptionRequest audioRequest = new AudioCreateTranscriptionRequest();
-        audioRequest.Model = Models.WhisperV1;
-        audioRequest.File = audioFile;
-        audioRequest.FileName = Path.GetFileName(filePath);
-        AudioCreateTranscriptionResponse audioResponse = await OpenAIService.Audio.CreateTranscription(audioRequest);
+        AudioCreateTranscriptionRequest audioRequest = new()
+        {
+            Model = Models.WhisperV1,
+            File = audioFile,
+            FileName = Path.GetFileName(filePath)
+        };
+
+        AudioCreateTranscriptionResponse audioResponse = await openAIService.Audio.CreateTranscription(audioRequest);
         return audioResponse.Text;
+    }
+
+    private void ConvertMediaUsingFffmpeg(string filePath, string newFilePath)
+    {
+        FFMpegArguments
+            .FromFileInput(filePath)
+            .OutputToFile(newFilePath)
+            .ProcessSynchronously();
     }
 }
