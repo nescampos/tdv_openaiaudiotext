ab1259728ac892f88db6ec6fa7967cfaf9984712
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2023-04-12 13:22:10 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using OpenAI.GPT3.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
-builder.Services.AddOpenAIService();
+var openAIKey = builder.Configuration["OpenAISecret"];
+builder.Services.AddOpenAIService(settings => { settings.ApiKey = openAIKey; });
 builder.Services.AddControllers();
 
 var app = builder.Build();
